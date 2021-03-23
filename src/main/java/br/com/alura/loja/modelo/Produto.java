5758b4762016f5d39b9a508c70e8683ea7979006
anderson-mnt <anderson.a.monteiro@outlook.com.br>
2021-03-22 23:06:15 -0300

    Curso Alura-JPA: Introdução ao Hibernate - Config EntityManager

---
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -0,0 +1,43 @@
+package br.com.alura.loja.modelo;
+
+import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Id
+	private Long id;
+	private String nome;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public BigDecimal getPreco() {
+		return preco;
+	}
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+	private String descricao;
+	private BigDecimal preco;
+}
